c4b92e63f8b9115711408d5e4051b08342b39d69
EnriqueEA <enrique19enrique@gmail.com>
2022-05-13 00:25:11 -0500

    cambios ojala finales

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    ignoreDefaultArgs: ['--disable-extensions']
+    headless: true,
+    args: ['--use-gl=egl'],
   }
 });
 
